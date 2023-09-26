241e9a751d7984d4ff80f8f56f16429d4e7fa02a
Rasibul <rasibul179@gmail.com>
2023-09-27 02:43:13 +0600

    show all button add

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,6 +4,7 @@ import DonationsCards from "./DonationsCard/DonationsCards";
 const Donation = () => {
     const [donation, setDontion] = useState([])
     const [noDonation, setEmtyDonation] = useState(false)
+    const [isShow,setisShow] = useState(false)
 
     useEffect(() => {
         const donationCards = JSON.parse(localStorage.getItem('donation'))
@@ -22,9 +23,13 @@ const Donation = () => {
                     :
                     <div>
                         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                           {
-                             donation?.map(card => <DonationsCards key={card.id} card={card}></DonationsCards>)
-                           }
+                            {
+                                isShow ? donation.map(card => <DonationsCards key={card.id} card={card}></DonationsCards>) :
+                                donation.slice(0,4).map(card => <DonationsCards key={card.id} card={card}></DonationsCards>)
+                            }
+                        </div>
+                        <div className="py-10">
+                            <button onClick={()=> setisShow(!isShow)} className="px-5   bg-green-200 block mx-auto">{isShow ? "" : "See All"}</button>
                         </div>
                     </div>
             }
