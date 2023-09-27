7eed583680885253b76c32566dbeb0d9a647392f
Rasibul <rasibul179@gmail.com>
2023-09-27 17:57:47 +0600

    css issu solve

--- src/Componenets/DonationCard/DonationCard.jsx
+++ src/Componenets/DonationCard/DonationCard.jsx
@@ -7,10 +7,9 @@ const DonationCard = ({ donationCard }) => {
     return (
         <Link to={`/donationCard/${id}`}>
             <div className="card card-compact  shadow-xl" style={{background:card_bg}}>
-
                 <img src={cover} alt="Shoes" />
                 <div className="card-body">
-                    <span className= " p-1 w-fit rounded font-medium" style={{background : category_bg}}>
+                    <span className= " p-1 w-fit rounded font-medium" style={{background : category_bg,color:color}}>
                     {category}
                     </span>
                     <h2 className=" text-md font-bold" style={{color :color}}>{title}</h2>
--- src/Pages/Donation/DonationsCard/DonationsCards.jsx
+++ src/Pages/Donation/DonationsCard/DonationsCards.jsx
@@ -1,18 +1,18 @@
 const DonationsCards = ({ card }) => {
-    const { id, cover, category, donate_amount, category_bg, card_bg, color, title, button_bg } = card || {}
+    const {  cover, category, donate_amount, category_bg, card_bg, color, title, button_bg } = card || {}
     return (
         <div>
             <div className="card card-side bg-base-100 shadow-xl" style={{ background: card_bg }}>
 
                 <img src={cover} alt="Movie" />
                 <div className="card-body">
-                    <span className=" p-1 w-fit rounded font-medium" style={{ background: category_bg, color: color }}>
+                    <span className=" p-1 w-fit rounded font-medium" style={{background : category_bg,color:color}}>
                         {category}
                     </span>
                     <h2 className="font-bold">{title}</h2>
                     <p className="" style={{color: color }}>$ {donate_amount}</p>
                     <div className="card-actions justify-start" >
-                        <button className="btn text-white" > View Details</button>
+                        <button className="btn text-white" style={{background:button_bg}} > View Details</button>
                     </div>
                 </div>
             </div>
